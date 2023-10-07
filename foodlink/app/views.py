0af1d285b5e54df7649ada573bd8a123e08524f0
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2023-10-07 15:00:39 -0400

    hooked up search function in backend

--- foodlink/app/views.py
+++ foodlink/app/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from call import search
+#helper functions are in views.py because django is weird
+import requests
+import json
+
+def search(ingredients):
+    s = "https://api.edamam.com/api/recipes/v2?type=public&q=" + ingredients + "&app_id=02b624bb&app_key=dd51796fcd6547eccd4141d1f2454ef4"
+    response = requests.get(s)
+    #print(response.status_code)
+    r1 = get_everything(response, 0)
+    r2 = get_everything(response, 1)
+    r3 = get_everything(response, 2)
+    r4 = get_everything(response, 3)
+    r5 = get_everything(response, 4)
+    recipies = [r1, r2, r3, r4, r5]
+    return recipies
+    
+    
+def get_calories(response, n):
+    return response.json()["hits"][n]["recipe"]["calories"]
+
+
+def get_label(response, n):
+    return response.json()["hits"][n]["recipe"]["label"]
+    
+
+def get_ingredients(response, n):
+    return response.json()["hits"][n]["recipe"]["ingredientLines"]
+
+def get_image(response, n):
+    return response.json()["hits"][n]["recipe"]["image"]
+
+
+def get_url(response, n):
+    return response.json()["hits"][n]["recipe"]["url"]
+
+
+def get_everything(response, n):
+    return {"label": get_label(response, n), "ingredients": get_ingredients(response, n), 
+             "calories": get_calories(response, n), "url": get_url(response, n), "image": get_image(response, n)}
+#image, nutrition, name, ingredients, url
+
 # Create your views here.
 
 def home_view(request, *args, **kargs):
@@ -34,5 +74,12 @@ def home_view(request, *args, **kargs):
             "5_info": recipes[4]['url'],
             "5_calories": recipes[4]['calories'],
         }
-        return render(request, 'recipe.html', context)
+        return render(request, 'results.html', context)
     return render(request, 'index.html', {})
+
+def about_view(request, *args, **kargs):
+    return render(request, 'aboutUs.html', {})
+'''
+def result_view(request, *args, **kargs):
+    return render(request, 'results.html', {})
+'''
--- foodlink/foodlink/urls.py
+++ foodlink/foodlink/urls.py
@@ -19,5 +19,7 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    #path('result/', result_view, name='about'),
 ]
